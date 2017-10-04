4a29f3e96da78ab64d893b790d71d9040dd2ae84
Martin Jullum <jullum@nr.no>
2017-10-04 09:23:12 +0200

    Test setup directly within parallel_testing setup to simplify

--- scripts/parallel_testing.R
+++ scripts/parallel_testing.R
@@ -4,7 +4,25 @@ rm(list=ls())
 library(devtools)
 install_github("martinju/lg")
 library(lg)
-source("scripts/profiling_setup.R")
+
+#### Test setup ####
+set.seed(123)
+n <- 500
+n.grid <- 1000
+dim.data <- 10
+
+x <- NULL
+grid <- NULL
+for (i in 1:dim.data){
+  x <- cbind(x,rnorm(n))
+  grid <- cbind(grid,rnorm(n))
+}
+
+lg_object <- lg(x)
+###############
+
+
+### Running the tests
 
 tt <- proc.time()
 test1.1=dlg(lg_object,grid)
