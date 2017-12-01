5cb659a7852f00783796d7799d0963ad548d6c3b
hotneim <haakon.otneim@gmail.com>
2017-12-01 16:31:13 +0100

    Finished README

--- R/main_function.R
+++ R/main_function.R
@@ -2,76 +2,76 @@
 # The main function that creates the 'lg'-object
 # ----------------------------------------------
 
-#' Create an \code{lg} object
+#'Create an \code{lg} object
 #'
-#' Create an \code{lg}-object, that can be used to estimate local Gussian
-#' correlations, unconditional and conditional densities, local partial
-#' correlation and for testing purposes.
+#'Create an \code{lg}-object, that can be used to estimate local Gussian
+#'correlations, unconditional and conditional densities, local partial
+#'correlation and for testing purposes.
 #'
-#' This is the main function in the package. It lets the user supply a data set
-#' and set a number of options, which is then used to prepare an \code{lg}
-#' object that can be supplied to other functions in the package, such as
-#' \code{dlg} (density estimation), \code{clg} (conditional density estimation).
-#' The details has been laid out in Otneim & Tjøstheim (2017a) and Otneim &
-#' Tjøstheim (2017b).
+#'This is the main function in the package. It lets the user supply a data set
+#'and set a number of options, which is then used to prepare an \code{lg} object
+#'that can be supplied to other functions in the package, such as \code{dlg}
+#'(density estimation), \code{clg} (conditional density estimation). The details
+#'has been laid out in Otneim & Tjøstheim (2017a) and Otneim & Tjøstheim
+#'(2017b).
 #'
-#' The papers mentioned above deal with the estimation of multivariate density
-#' functions and conditional density functions. The idea is to fit a
-#' multivariate Normal locally to the unknown density function by first
-#' transforming the data to marginal standard normality, and then estimate the
-#' local correlations \strong{pairwise}. The local means and local standard
-#' deviations are held fixed and constantly equal to 0 and 1 respectively to
-#' reflect the knowledge that the marginals are approximetaly standard normal.
-#' Use \code{est_method = "1par"} for this strategy, which means that we only
-#' estimate one local parameter (the correlation) for each pair, and note that
-#' this method requires marginally standard normal data. If \code{est_method =
-#' "1par"} and \code{transform_to_marginal_normality = FALSE} the function will
-#' throw a warning. It might be okay though, if you know that the data are
-#' marginally standard normal already.
+#'The papers mentioned above deal with the estimation of multivariate density
+#'functions and conditional density functions. The idea is to fit a multivariate
+#'Normal locally to the unknown density function by first transforming the data
+#'to marginal standard normality, and then estimate the local correlations
+#'\strong{pairwise}. The local means and local standard deviations are held
+#'fixed and constantly equal to 0 and 1 respectively to reflect the knowledge
+#'that the marginals are approximetaly standard normal. Use \code{est_method =
+#'"1par"} for this strategy, which means that we only estimate one local
+#'parameter (the correlation) for each pair, and note that this method requires
+#'marginally standard normal data. If \code{est_method = "1par"} and
+#'\code{transform_to_marginal_normality = FALSE} the function will throw a
+#'warning. It might be okay though, if you know that the data are marginally
+#'standard normal already.
 #'
-#' The second option is \code{est_method = "5par_marginals_fixed"} which is more
-#' flexible than \code{"1par"}. This method will estimate univariate local
-#' Gaussian fits to each marginal, thus producing local estmates of the local
-#' means: \eqn{\mu_i(x_i)} and \eqn{\sigma_i(x_i)} that will be held fixed in
-#' the next step when the \strong{pairwise} local correlations are estimated.
-#' This method can in many situations provide a better fit, even if the
-#' marginals are standard normal. It also opens up for creating a multivariate
-#' locally Gaussian fit to any density without having to transform the marginals
-#' if you for some reason want to avoid that.
+#'The second option is \code{est_method = "5par_marginals_fixed"} which is more
+#'flexible than \code{"1par"}. This method will estimate univariate local
+#'Gaussian fits to each marginal, thus producing local estmates of the local
+#'means: \eqn{\mu_i(x_i)} and \eqn{\sigma_i(x_i)} that will be held fixed in the
+#'next step when the \strong{pairwise} local correlations are estimated. This
+#'method can in many situations provide a better fit, even if the marginals are
+#'standard normal. It also opens up for creating a multivariate locally Gaussian
+#'fit to any density without having to transform the marginals if you for some
+#'reason want to avoid that.
 #'
-#' The third option is \code{est_method = "5par"}, which is a full nonparametric
-#' locally Gaussian fit of a bivariate density as laid out and used by Tjøstheim
-#' & Hufthammer (2013) and others. This is simply a wrapper for the
-#' \code{localgauss}-package by Berentsen et.al. (2014).
+#'The third option is \code{est_method = "5par"}, which is a full nonparametric
+#'locally Gaussian fit of a bivariate density as laid out and used by Tjøstheim
+#'& Hufthammer (2013) and others. This is simply a wrapper for the
+#'\code{localgauss}-package by Berentsen et.al. (2014).
 #'
-#' @param x A matrix or data frame with data, on column per variable, one row
-#'   per observation.
-#' @param bw_method The method used for bandwidth selection. Must be either
-#'   \code{"cv"} (cross-validation, slow, but accurate) or \code{"plugin"}
-#'   (fast, but crude).
-#' @param est_method The estimation method, must be either "1par", "5par" or
-#'   "5par_marginals_fixed" (see details).
-#' @param transform_to_marginal_normality Logical, \code{TRUE} if we want to
-#'   transform our data to marginal standard normality. This is assumed by
-#'   method "1par", but can of course be skipped using this argument if it has
-#'   been done already.
-#' @param bw Bandwidth object if it has already been calculated.
-#' @param plugin_constant_marginal The constant \code{c} in \code{cn^a} used for
-#'   finding the plugin bandwidth for locally Gaussian marginal density
-#'   estimates, which we need if estimation method is "5par_marginals_fixed".
-#' @param plugin_exponent_marginal The constant \code{a} in \code{cn^a} used for
-#'   finding the plugin bandwidth for locally Gaussian marginal density
-#'   estimates, which we need if estimation method is "5par_marginals_fixed".
-#' @param plugin_constant_joint The constant \code{c} in \code{cn^a} used for
-#'   finding the plugin bandwidth for estimating the pairwise local Gaussian
-#'   correlation between two variables.
-#' @param plugin_exponent_joint The constant \code{a} in \code{cn^a} used for
-#'   finding the plugin bandwidth for estimating the pairwise local Gaussian
-#'   correlation between two variables.
-#' @param tol_marginal The absolute tolerance in the optimization for finding
-#'   the marginal bandwidths, passed on to the \code{optim}-function.
-#' @param tol_joint The absolute tolerance in the optimization for finding the
-#'   joint bandwidths. Passed on to the \code{optim}-function.
+#'@param x A matrix or data frame with data, on column per variable, one row per
+#'  observation.
+#'@param bw_method The method used for bandwidth selection. Must be either
+#'  \code{"cv"} (cross-validation, slow, but accurate) or \code{"plugin"} (fast,
+#'  but crude).
+#'@param est_method The estimation method, must be either "1par", "5par" or
+#'  "5par_marginals_fixed" (see details).
+#'@param transform_to_marginal_normality Logical, \code{TRUE} if we want to
+#'  transform our data to marginal standard normality. This is assumed by method
+#'  "1par", but can of course be skipped using this argument if it has been done
+#'  already.
+#'@param bw Bandwidth object if it has already been calculated.
+#'@param plugin_constant_marginal The constant \code{c} in \code{cn^a} used for
+#'  finding the plugin bandwidth for locally Gaussian marginal density
+#'  estimates, which we need if estimation method is "5par_marginals_fixed".
+#'@param plugin_exponent_marginal The constant \code{a} in \code{cn^a} used for
+#'  finding the plugin bandwidth for locally Gaussian marginal density
+#'  estimates, which we need if estimation method is "5par_marginals_fixed".
+#'@param plugin_constant_joint The constant \code{c} in \code{cn^a} used for
+#'  finding the plugin bandwidth for estimating the pairwise local Gaussian
+#'  correlation between two variables.
+#'@param plugin_exponent_joint The constant \code{a} in \code{cn^a} used for
+#'  finding the plugin bandwidth for estimating the pairwise local Gaussian
+#'  correlation between two variables.
+#'@param tol_marginal The absolute tolerance in the optimization for finding the
+#'  marginal bandwidths, passed on to the \code{optim}-function.
+#'@param tol_joint The absolute tolerance in the optimization for finding the
+#'  joint bandwidths. Passed on to the \code{optim}-function.
 #'
 #' @examples
 #'   x <- cbind(rnorm(100), rnorm(100), rnorm(100))
@@ -104,23 +104,26 @@
 #'   grid2 <- matrix(-3:3, ncol = 1)
 #'   c_dens_est <- clg(lg_object1, grid = grid2, condition = c(1, 0))
 #'
-#' @references
+#'@references
 #'
-#' Berentsen, Geir Drage, Tore Selland Kleppe, and Dag Tjøstheim. "Introducing
-#' localgauss, an R package for estimating and visualizing local Gaussian
-#' correlation." Journal of Statistical Software 56.1 (2014): 1-18.
+#'Berentsen, Geir Drage, Tore Selland Kleppe, and Dag Tjøstheim. "Introducing
+#'localgauss, an R package for estimating and visualizing local Gaussian
+#'correlation." Journal of Statistical Software 56.1 (2014): 1-18.
 #'
-#' Hufthammer, Karl Ove, and Dag Tjøstheim. "Local Gaussian Likelihood and Local
-#' Gaussian Correlation" PhD Thesis of Karl Ove Hufthammer, University of
-#' Bergen, 2009.
+#'Hufthammer, Karl Ove, and Dag Tjøstheim. "Local Gaussian Likelihood and Local
+#'Gaussian Correlation" PhD Thesis of Karl Ove Hufthammer, University of Bergen,
+#'2009.
 #'
-#' Otneim, Håkon, and Dag Tjøstheim. "The locally gaussian density estimator for
-#' multivariate data." Statistics and Computing 27, no. 6 (2017a): 1595-1616.
+#'Otneim, Håkon, and Dag Tjøstheim. "The locally gaussian density estimator for
+#'multivariate data." Statistics and Computing 27, no. 6 (2017a): 1595-1616.
 #'
-#' Otneim, Håkon, and Dag Tjøstheim. "Conditional density estimation using the
-#' local Gaussian correlation" Statistics and Computing (2017b): 1-19.
+#'Otneim, Håkon, and Dag Tjøstheim. "Conditional density estimation using the
+#'local Gaussian correlation" Statistics and Computing (2017b): 1-19.
 #'
-#' @export
+#'Tjøstheim, D., & Hufthammer, K. O. (2013). Local Gaussian correlation: a new
+#'measure of dependence. Journal of Econometrics, 172(1), 33-48.
+#'
+#'@export
 lg_main <- function(x,
                bw_method = "plugin",
                est_method = "1par",
--- README.md
+++ README.md
@@ -5,18 +5,90 @@ The lg package for calculating local Gaussian correlations in multivariate appli
 
 Otneim and Tjøstheim (2017a) describes a new method for estimating multivariate density functions using the concept of local Gaussian correlations. Otneim and Tjøstheim (2017b) expands the idea to the estimation of *conditional* density functions. This package, written for the R programming language, provides a simple interface for implementing these methods in practical problems.
 
-Example
-=======
+Construction of the lg-object
+=============================
 
 Let us illustrate the use of this package by looking at the built-in data set of daily closing prices of 4 major European stock indices in the period 1991-1998. We load the data and transform them to daily returns:
 
 ``` r
 data(EuStockMarkets)
 x <- apply(EuStockMarkets, 2, function(x) diff(log(x)))
+
+# Remove the days where at leas one index did not move at all
+x <- x[!apply(x, 1, function(x) any(x == 0)),]
 ```
 
-Installation instructions
-=========================
+When using this package, the first task is always to create an lg-object using the lg\_main()-function. This object contains all the estimation parameters that will be used in the estimation step, including the bandwidths. There are three main parameters that one can tune:
 
-Overview
-========
+-   **The bandwidth selection method**: The bandwidth selection method is controlled through the argument *bw\_method*. It can take one of two values:
+    -   Use *bw\_method = "cv"* to use the cross-validation routine described by Otneim and Tjøstheim (2017a). Depending on your system, this method is fairly slow and may take several minutes even for moderately sized data sets.
+    -   Use *bw\_method = "plugin"* for plugin bandwidths. This is the **default** method and very quick. It simply sets all bandwidths equal to 1.75*n^(-1/6), which is derived from the asymptotic convergence rates for the local Gaussian correlations. Both numbers (1.75 and -1/6) can be set manually (see documentation of the *lg\_main()\*-function).
+-   **The estimation method:** The method of estimation is controlled through the argument *est\_method*. It can tak one of three values:
+    -   Otneim and Tjøstheim (2017a) uses a simplified method for multivariate density estimation. The density estimate is a locally Gaussian distribution, with correlations being estimated *locally* and *pairwise*. The data is transformed for marginal standard normality (see next point) and as a consequence, we fix the means and standard deviations to 0 and 1 respectively. To use this estimation method, write *est\_method = "1par"*. This is the **default** method.
+    -   Set *est\_method = "5par\_marginals\_fixed"* to estimate local means and local standard deviations marginally, as well as the pairwise local correlations. This is a more flexible method, but its theoretical properties are not (yet) fully understood. This configuration allows for the estimation of multivariate density functions without having to transform the data.
+    -   The option *est\_method = "5par"* is reserved to bivariate problems, and is a fully nonparametric estimation method as laid out by Tjøstheim & Hufthammer (2013). This will simply invoke the *localgauss* package (Berentsen et. al., ).
+-   **Transformation of the marginals:** This is controlled by the logical argument *transform\_to\_marginal\_normality*. If true, the marginals are transformed to marginal standard normality according to Otneim and Tjøstheim (2017a). This is the **default** method.
+
+See the documentation of the *lg\_main()*-function for further details. We can now construct the lg-object using the default configuration by running
+
+``` r
+library(lg)
+lg_object <- lg_main(x)
+```
+
+Estimation of density functions
+===============================
+
+We can then specify a set of grid points and estimate the probability density function of *x* using the *dlg()*-function. We choose a set of grid ponts that go diagonally through R^4, estimate, and plot the result as follows:
+
+``` r
+grid <- matrix(rep(seq(-.03, .03, length.out = 100), 4), ncol = 4, byrow = FALSE)
+density_estimate <- dlg(lg_object = lg_object, grid = grid)
+plot(grid[,1], density_estimate$f_est, type = "l",
+     xlab = "Diagonal grid point", ylab = "Estimated density")
+```
+
+![](README-estimate-density-1.png)
+
+Estimation of conditional densities
+===================================
+
+If we want to calculate conditional density functions, we must take to notice the *order* of the columns in our data set (x). This is because the estimations routine, implemented in the *clg()*-function, will always assume that the independent variables come first. Looking at the top of our data set:
+
+``` r
+head(x)
+#>               DAX          SMI          CAC         FTSE
+#> [1,] -0.009326550  0.006178360 -0.012658756  0.006770286
+#> [2,] -0.004422175 -0.005880448 -0.018740638 -0.004889587
+#> [3,]  0.009003794  0.003271184 -0.005779182  0.009027020
+#> [4,] -0.001778217  0.001483372  0.008743353  0.005771847
+#> [5,] -0.004676712 -0.008933417 -0.005120160 -0.007230164
+#> [6,]  0.012427042  0.006737244  0.011714353  0.008517217
+```
+
+we see that DAX comes first. Say that we want to estimate the conditional density of DAX, given that SMI = CAC = FTSE = 0. We do that by running
+
+``` r
+grid <- matrix(seq(-.03, .03, length.out = 100), ncol = 1)   # The grid must be a matrix
+condition <- c(0, 0, 0)                                      # Value of dependent variables
+cond_dens_est <- clg(lg_object = lg_object, 
+                     grid = grid,
+                     condition = condition)
+plot(grid, cond_dens_est$f_est, type = "l",
+     xlab = "DAX", ylab = "Estimated conditional density")
+```
+
+![](README-conditional-1.png)
+
+If we want to estimate the conditional density of CAC and FTSE given DAX and SMI, for example, we must first shuffle the data so that CAC and FTSE come first, and supply the conditional value for DAX and SMI through the vector *condition*, now having two elements.
+
+References
+==========
+
+Berentsen, Geir Drage, Tore Selland Kleppe, and Dag Tjøstheim. "Introducing localgauss, an R package for estimating and visualizing local Gaussian correlation." Journal of Statistical Software 56.1 (2014): 1-18.
+
+Otneim, Håkon, and Dag Tjøstheim. "The locally Gaussian density estimator for multivariate data." Statistics and Computing 27, no. 6 (2017a): 1595-1616.
+
+Otneim, Håkon, and Dag Tjøstheim. "Conditional density estimation using the local Gaussian correlation" Statistics and Computing (2017b): 1-19.
+
+Tjøstheim, Dag, & Hufthammer, Karl Ove (2013). Local Gaussian correlation: a new measure of dependence. Journal of Econometrics, 172(1), 33-48.
